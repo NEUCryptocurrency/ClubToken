948f10d70839d294aeb0af9bfdd6da5293aab9a0
wstenzel <wills.stenzel@gmail.com>
2019-01-07 12:16:42 -0500

    removed TestVoting so it can be sumbitted to block tester

--- TestVoting.sol
+++ TestVoting.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.18;
+/* pragma solidity ^0.4.18;
 
 import "./Voting.sol";
 
@@ -8,4 +8,4 @@ contract TestVoting is Voting {
     address voter = 0x3f5Af07E277BBAf4BFA448e29CE944968d52B8ce;
     addChoice();
   }
-}
+} */
